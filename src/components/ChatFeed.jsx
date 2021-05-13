facd134a7ad23204b4d831fec212e3103e7dcc0d
Samarth Shah <samarth.s@ahduni.edu.in>
2021-05-13 22:07:23 +0530

    updated components

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -3,12 +3,53 @@ import myMessage from './myMessage'
 import theirMessage from './theirMessage'
 
 const ChatFeed = (props) => {
-  console.log(props);
+  const { chats, activeChat, userName, messages} = props;
+  const chat = chats && chats[activeChat];
+  console.log(chat, userName, messages );
+  
+  const renderMessages = () =>{
+    const keys = Object.keys(messages);
 
+    return keys.map((key,index) => {
+        const message = messages[key];
+        const lastMessageKey = index === 0 ? null : keys[index-1];
+        const isMyMessage = userName === message.sender.username;
+
+        return(
+          <div key = {`msg_${index}`} style={{width: `100`}}>
+              <div className="message-block">
+                {
+                  isMyMessage
+                  ? <myMessage message={message}/>
+                  : <theirMessage message ={message} lastMessage= {messages[lastMessageKey]}/>
+                }
+              </div>
+              <div className="read-receipts" style={{marginRight: isMyMessage ? '18PX': '0px', marginLeft: isMyMessage ? '0px': '68px'}}>
+              read-receipts
+              </div>
+          </div>
+        );
+
+    })
+  }
+
+  if(!chat) return 'Loading..!';
   return(
-    <div>
-      ChatFeed
-    </div>
+      <div className="chat-feed">
+        <div className="chat-title-container">
+          <div className="chat-title">
+            {chat?.title}
+          </div>
+          <div className="chat-subtitle">
+            {chat.people.map((person) => ` ${person.person.userName}`)}
+          </div>
+        </div>
+        {renderMessages()}
+        <div style={{height: '100px'}}/>
+          <div className="message-form-container">
+            <messageForm {... props} chatId={activeChat} />
+          </div>
+      </div>
   )
 }
 
--- src/components/theirMessage.jsx
+++ src/components/theirMessage.jsx
@@ -1,7 +1,24 @@
-const theirMessage = () => {
-  return(
-    <div>
-     Their Message  
+const theirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser = !lastMessage || lastMessage.sender.username !== message.sender.username;
+  return (
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avater"
+          style={{ backgroundImage: `url(${message?.sender?.avater})` }} />
+      )}
+      {message?.attachements?.length > 0
+        ? (
+          <img src={message.attachments[0].file}
+            alt="message-attachment"
+            className="message-image"
+            style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px'}} />
+        ) : (
+          <div className="message" style={{ float: 'left', backgroundColor: '##CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px'}}>
+            {message.text}
+          </div>
+        )
+      }
     </div>
   )
 }
