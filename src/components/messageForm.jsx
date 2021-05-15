e44d67f05c45a98e44234592df6f262b101ff56b
Samarth Shah <samarth.s@ahduni.edu.in>
2021-05-15 14:10:46 +0530

    updated messageFom

--- src/components/messageForm.jsx
+++ src/components/messageForm.jsx
@@ -1,8 +1,30 @@
-const messageForm = () => {
+import { userState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+const messageForm = (props) => {
+  const [value, setValue] = userState('');
+  const { chatID, creds } = props;
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const text = value.trim();
+
+      if(text.length > 0) sendMessage(creds, chatID, {text});
+  }
+  const handleChange = (event) => {
+      setValue(event.target.value);
+      isTyping(props, chatID);
+  }
   return(
-    <div>
-      Message Form  
-    </div>
+    <from className="message-form" onSubmit={handleSubmit}>
+<input 
+  className="message-input"
+  placeholder="Send a message... "
+  value={value}
+  onChange = {handleChange}
+  onSubmit={handleSubmit}
+/>
+    </from>
   )
 }
 
